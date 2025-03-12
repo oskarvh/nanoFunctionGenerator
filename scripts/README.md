e1782ffb27de79e689730d939cb9f36c9bab6dfc
Oskar von Heideken <oskar.von.heideken@gmail.com>
2025-03-11 22:05:26 -0700

    Added script to calculate the PWM signal

---
+++ scripts/README.md
@@ -0,0 +1,17 @@
+# Scripts
+This directory contains some of the scripts useful for this project.
+
+### Usage
+0. Install Python 3.10 or newer
+1. Go to this directory if not done so already (`cd scripts`)
+2. Create and activate a virtual environment with python3: `mkdir .venv; python3 -m venv .venv; source .venv/bin/activate`
+3. Install the dependencies: `python3 -m pip install -r requirements.txt`
+4. This installs the dependencies required for any of the scripts in this directory, so you can go ahead and just run anything!
+
+## Sine wave modulation calculation
+The `sine_wave_modulation.py` script generates a set of plots and maths related to 
+modulating a sine wave on a PWM signal. 
+
+### Usage
+0. Activate the virtual environment (described in scripts/usage above)
+1. Run the script: `python3 sine_wave_modulation.py`
---
+++ scripts/sine_wave_modulation.py
@@ -0,0 +1,126 @@
+"""
+MIT License
+
+Copyright (c) 2025 Oskar von Heideken
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+This script is a simple example of how to modulate a sine wave using a PWM signal. 
+It's intended to be messed around with such that the user can get a feel for how 
+this modulation scheme works.
+"""
+
+# Requirements
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+def parse_args():
+    """
+    Parse the arguments
+    :return: args
+    """
+    parser = argparse.ArgumentParser(description="Modulate a sine wave using a PWM signal.")
+    parser.add_argument("--pwm_frequency_hz", type=int, default=1000, required=False, help="PWM freqency in Hz. This is the frequency of the PWM clock.")
+    parser.add_argument("--sine_wave_frequency_hz", type=int, default=10, required=False, help="Sine wave frequency in Hz.")
+    return parser.parse_args()
+
+def generate_pwm_sine_wave():
+    """
+    Generate a sine wave modulated by a PWM signal.
+    """
+    # Constants for RP2040 and some other things that needn't be changed
+    MAX_PWM_COUNT = 65535
+    PWM_FREQUENCY_RP2040 = 125E6 # 125 MHz
+    PWM_PERIOD = 1/PWM_FREQUENCY_RP2040
+    NUM_PERIODS = 2 # Number of periods to generate
+
+    # TODO: These should be argument later on
+    num_points_per_period = 100 # This is the wrap count of the PWM counter
+    dc_offset = 0.5 # Normalized DC offset
+    amplitude = 0.5 # Normalized amplitude
+    sine_wave_frequency_hz = 10000
+    phase_offset_rad = np.pi*0.0 # Phase offset in radians
+
+    sine_wave_period_time = NUM_PERIODS/sine_wave_frequency_hz
+    pwm_period_time = NUM_PERIODS*num_points_per_period/PWM_FREQUENCY_RP2040
+    num_pwm_periods_per_sine_wave_period = int(sine_wave_period_time/pwm_period_time)
+    num_pwm_samples = num_pwm_periods_per_sine_wave_period*num_points_per_period
+
+    sine_wave_time = np.linspace(0, NUM_PERIODS/sine_wave_frequency_hz, int(num_pwm_samples/num_points_per_period))
+    # Generate the sine wave
+    w = 2*np.pi*sine_wave_frequency_hz
+    sine_wave = amplitude*np.sin(w*sine_wave_time + phase_offset_rad)+dc_offset
+
+    # Given the sine wave, we can now modulate the duty cycle of the PWM signal
+    # with the sine wave, since the length of the vector is equal to the points per period
+    duty_cycle = sine_wave
+    
+    # Now generate the PWM signal. For each num_points_per_period, we'll sample the sine wave
+    # and set the duty cycle of the PWM signal to that value
+    
+    print(f"{sine_wave_period_time=}, {pwm_period_time=}, {num_pwm_periods_per_sine_wave_period=}")
+    pwm_time = np.linspace(0, NUM_PERIODS/sine_wave_frequency_hz, num_pwm_samples)
+
+    sine_wave_idx = 0
+    pwm_signal = np.zeros(num_pwm_samples)
+    duty_cycle_signal = np.zeros(num_pwm_samples)
+    duty_cycle = sine_wave[0]
+    pwm_signal_value = 1
+    duty_cycle_set_point = 0
+    set_point_counter = 0
+    for i in range(0, num_pwm_samples):
+        if i % num_points_per_period == 0:
+            # Sample the sine wave
+            duty_cycle = sine_wave[sine_wave_idx]
+            sine_wave_idx += 1
+
+            # Reset the PWM signals
+            pwm_signal_value = 1
+            set_point_counter = 0
+
+            # Calcualte the duty cycle set point
+            duty_cycle_set_point = int(num_points_per_period*duty_cycle)
+        duty_cycle_signal[i] = duty_cycle
+        set_point_counter += 1
+        if set_point_counter > duty_cycle_set_point:
+            pwm_signal_value = 0
+        pwm_signal[i] = pwm_signal_value
+    
+    plt.subplot(211)
+    pwm_plot = plt.plot(pwm_time, pwm_signal)
+    plt.legend(["PWM signal"], loc = "upper right")
+    plt.subplot(212)
+    sine_plot = plt.plot(sine_wave_time, sine_wave)
+    dc_plot = plt.plot(pwm_time, duty_cycle_signal)
+    plt.legend(["Sine Wave", "PWM Signal duty cycle value"], loc = "upper right")
+    plt.grid()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Normalized Amplitude")
+    plt.show()
+
+
+    pass
+
+if __name__ == "__main__":
+    # Parse arguments
+    args = parse_args()
+    generate_pwm_sine_wave()
+
+    
